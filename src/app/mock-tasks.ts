96d1a42419dbf85c98d047b14bb9bd065a6c9139
Nippur7 <nippur.argento@gmail.com>
2022-11-15 23:27:31 -0300

    trabajando sobre las tarjetas tasks

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -2,25 +2,25 @@ import { Task } from "./Task"
 export const TASKS:Task[] = [
     {
         id: 1,
-        text: "Terminar primer modulo de Angular"
+        text: "Terminar primer modulo de Angular",
         day: 'Agosto 5 a las 12:00',
         reminder: true
     },
     {
         id: 2,
-        text: "Hacer las compras para la cena"
+        text: "Hacer las compras para la cena",
         day: 'Agosto 5 a las 17:00',
         reminder: true
     },
     {
         id: 3,
-        text: "Investigar sobre Boostrap"
+        text: "Investigar sobre Boostrap",
         day: 'Agosto 5 a las 20:00',
         reminder: false
     },
     {
         id: 4,
-        text: "Dar de comer a pepa"
+        text: "Dar de comer a pepa",
         day: 'Agosto 5 a las 22:00',
         reminder: false
     }
